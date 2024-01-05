d687acbfbdaa14fba5051dde4fd76eb5401068c6
binod009 <ramguy007@gmail.com>
2024-01-05 22:07:59 +0545

    publishdir change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import "./App.css";
 
 import HeroSection from "./components/HeroSection";
 
-import Services from "./components/Services";
-import Testomonial from "./components/Testomonial";
+// import Services from "./components/Services";
+// import Testomonial from "./components/Testomonial";
 
 import Footer from "./components/Footer";
 import EventSection from "./components/EventSection";
