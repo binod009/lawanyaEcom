bf38d55372b381f4d15b458db3d45a9add2e2c29
binod009 <ramguy007@gmail.com>
2024-02-18 19:40:11 +0545

    changes

--- api/config/config.js
+++ api/config/config.js
@@ -10,7 +10,7 @@ const Config = {
   },
   DB: {
     PROTOCOL: "mongodb",
-    HOST: "127.0.0.1",
+    HOST: "localhost",
     NAME: "lawanya",
     USER: "",
     PWD: "",
@@ -24,3 +24,5 @@ const Config = {
 //   }@cluster0.8brv6gn.mongodb.net/`;
 // }
 module.exports = Config;
+
+// 127.0.0.1
