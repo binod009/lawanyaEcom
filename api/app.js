d5a9a615447f27b8bb8b4e9e791d8d2a622b0fb2
binod009 <ramguy007@gmail.com>
2024-03-08 12:15:02 +0545

    port0000

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const routes = require("./route/index");
 
 const app = express();
 app.use(helmet());
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 //This will allow the frontend to get access my backend {files ,image}
 app.use((req, res, next) => {
